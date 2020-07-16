a410d57ada31e522c74a80de4f5ff505fa70b3ac
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-07-16 05:17:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,9 +12,11 @@ WIN_COMBINATIONS=[
   [6, 7, 8],
   [0, 3, 6],
   [1, 4, 7],
-  
-  
-  
-  
-  
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
   ]
+  
+  def won?(board)
+    WIN_COMBINATIONS.each {win_combo|
+    
