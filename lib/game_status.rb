7361a98dfe11d0540e19ef124b15526eda623767
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-07-16 05:25:28 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,23 +17,23 @@ WIN_COMBINATIONS=[
   [6, 4, 2]
   ]
   
-  def won?(board)
-    WIN_COMBINATIONS.each {win_combo|
-    win_index_1=win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
+def won?(board)
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
 
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-    
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
-    elsif position_1 == "0" && position_2 == "0" && position_3 == "0"
-      return win_combination
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
     end
-    }
-    return false
+  }
+  return false
 end
   
 def full?(board)
