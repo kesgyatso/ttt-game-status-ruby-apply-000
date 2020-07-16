4b0f2108687d13df8ab9aafceb2afa181a609b17
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-07-16 05:24:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -34,8 +34,19 @@ WIN_COMBINATIONS=[
     end
     }
     return false
-  end
+end
   
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
   
   
     
